b5f75b7959750fa53063ad4ccf8f2d7ebf0340e7
popov_vv <popov_vv@teleformis.ru>
2022-04-16 08:36:51 +0300

    Поправил поле с описанием.

--- src/products.js
+++ src/products.js
@@ -57,7 +57,12 @@ export const ProductEdit = (props) => (
         <SelectInput optionText="name" validate={[required()]} />
       </ReferenceInput>
       <TextInput source="title" validate={[required()]} />
-      <TextInput source="description" validate={[required()]} />
+      <TextInput
+        source="description"
+        validate={[required()]}
+        fullWidth
+        multiline
+      />
       <NumberInput source="price" validate={[required()]} />
       <NumberInput source="discount" validate={[required()]} />
     </SimpleForm>
